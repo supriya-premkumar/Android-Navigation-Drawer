4e2e8f457e0b54b712fff71369405efa0e5a009b
supriyapremkumar <spremkum@ucsc.edu>
2016-11-03 18:46:13 -0700

    Initial Commit

---
+++ app/src/main/java/com/codepath/android/navigationdrawerexercise/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.android.navigationdrawerexercise.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codepath.android.navigationdrawerexercise.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
